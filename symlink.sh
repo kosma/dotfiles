905e21a88ff5b731d7e4155b204252b59b9c587b
Kosma Moczek <kosma@kosma.pl>
2013-05-13 21:13:23 +0200

    dont make unneccessary assumptions

--- symlink.sh
+++ symlink.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
 set -e
-
-if [[ "$PWD" != "$HOME/dotfiles" ]]; then
-    echo "This repository must be cloned in ~/dotfiles." 1>&2
-    exit 1
-fi
-
 shopt -s dotglob
 shopt -s nullglob
 
@@ -21,7 +15,7 @@ for file in .[^.]?*; do
     if ! [[ -e "$target" ]]; then
         target="$HOME/$file"
         echo "Install: $file -> $target"
-        ln -s "dotfiles/$file" "$target"
+        ln -s "$PWD/$file" "$target"
     elif [[ -L "$target" ]]; then
         echo "Skip: $file"
     else
